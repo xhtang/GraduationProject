36cbf56ffb01344f8235531f825dffe9b4997895
xhtang <15302010031@fudan.edu.cn>
2018-12-12 19:37:09 +0800

    teacher delete course 0..0

--- src/main/java/fudan/ss/mindmapbackend/controller/CourseController.java
+++ src/main/java/fudan/ss/mindmapbackend/controller/CourseController.java
@@ -97,7 +97,7 @@ public class CourseController {
         return s;
     }
 
-    @RequestMapping(value = "/delete_course_student/{user_name}/{course_id}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/delete_course_teacher/{user_name}/{course_id}", method = RequestMethod.DELETE)
     public Success delete_course_teacher(@PathVariable String user_name, @PathVariable String course_id) {
         Success s = new Success();
         s.setSuccess(courseService.deleteCourse(user_name, course_id));
